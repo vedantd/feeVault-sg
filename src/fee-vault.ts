8481597f1182d53460eeb87d6987b23d1bd32189
vedant <you@example.com>
2022-11-03 21:09:56 +0530

    feeVault subgraph

--- src/fee-vault.ts
+++ src/fee-vault.ts
@@ -6,53 +6,56 @@ import {
   OwnershipTransferred,
   Withdraw
 } from "../generated/feeVault/feeVault"
-import { ExampleEntity } from "../generated/schema"
+
+import { DepositEntity, WithdrawEntity, OwnershipTransferredEntity, InitializedEntity } from "../generated/schema"
+
 
 export function handleDeposit(event: Deposit): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  let entity = new DepositEntity(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+  )
+  entity.from = event.params.from.toHexString();
+  entity.amount = event.params.amount;
+  entity.blockNum = event.block.number;
+  entity.txHash = event.transaction.hash.toHex();
+  entity.blockTime = event.block.timestamp;
+  entity.save();
+  }  
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.from = event.params.from
-  entity.amount = event.params.amount
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.addressStore(...)
-  // - contract.owner(...)
+export function handleInitialized(event: Initialized): void {
+  let entity = new InitializedEntity(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+  ) 
+  entity.version = event.params.version;
+  entity.blockNum = event.block.number;
+  entity.txHash = event.transaction.hash.toHex();
+  entity.blockTime = event.block.timestamp;
+  entity.save();
 }
 
-export function handleInitialized(event: Initialized): void {}
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+  let entity = new OwnershipTransferredEntity(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+  ) 
+  entity.previousOwner = event.params.previousOwner.toHex();
+  entity.newOwner = event.params.newOwner.toHex();
+  entity.blockNum = event.block.number;
+  entity.txHash = event.transaction.hash.toHex();
+  entity.blockTime = event.block.timestamp;
+  entity.save();
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+}
 
-export function handleWithdraw(event: Withdraw): void {}
+export function handleWithdraw(event: Withdraw): void {
+  let entity = new WithdrawEntity(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+  ) 
+  entity.from = event.params.from.toHexString();
+  entity.to = event.params.to.toHexString();
+  entity.amount = event.params.amount;
+  entity.blockNum = event.block.number;
+  entity.txHash = event.transaction.hash.toHex();
+  entity.blockTime = event.block.timestamp;
+  entity.save();
+}
